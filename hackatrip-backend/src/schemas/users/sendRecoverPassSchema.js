be394dd4a1824858bd2ead61da58f98fa593a985
Sara González <saragonzalezrmrez@gmail.com>
2025-07-25 18:22:54 +0200

    eliminación carpeta

--- hackatrip-backend/src/schemas/users/sendRecoverPassSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from 'joi';
-
-import joiErrorMessages from '../joiErrorMessages.js';
-
-const sendRecoverPassSchema = joi.object({
-    email: joi.string().email().required().messages(joiErrorMessages),
-});
-
-export default sendRecoverPassSchema;
