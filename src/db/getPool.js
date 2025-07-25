4b4879b1314c09c764b4181da8fed9c89d6a19d9
Sara González <saragonzalezrmrez@gmail.com>
2025-07-25 19:15:48 +0200

    cambios pool

--- src/db/getPool.js
+++ src/db/getPool.js
@@ -5,6 +5,7 @@ import { MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB } from '../../env.js';
 let pool;
 
 const getPool = async () => {
+
     try {
         if (!pool) {
             const tempPool = await mysql.createPool({
@@ -12,8 +13,9 @@ const getPool = async () => {
                 user: MYSQL_USER,
                 password: MYSQL_PASS,
             });
-
+            
             await tempPool.query(`CREATE DATABASE IF NOT EXISTS ${MYSQL_DB}`);
+            console.log(pool);
 
             pool = await mysql.createPool({
                 host: MYSQL_HOST,
